e4f3b1396530940c064284ae2bf7290bb8d60e76
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-02-26 16:37:50 +0000

    Getting Input from Yargs and Simplified Input with Yargs.

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+let command = argv._[0];
+console.log('Command:', command);
+console.log('Yargs:', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'read') {
+  notes.readNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else if (command === 'list') {
+  notes.getAll();
+} else {
+  console.log('Command not recognised');
+}
+
+/*********************************************************
+const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
 
-let filteredArray = _.uniq(['Alan', 1, 'Alan', 1, 2, 3, 4]);
+let filteredArray = _.uniq(['Alan', 1, 'Mike', 1, 2, 3, 4]);
 console.log(filteredArray);
 
 console.log(_.isString(true));
@@ -18,7 +42,7 @@ console.log(res);
 
 let user = os.userInfo();
 
-// Option One
+Option One
 fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
   if (err) {
     console.log('Unable to write to file');
@@ -27,3 +51,5 @@ fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, 
 
 // // Option Two
 // fs.appendFileSync('greetings.txt', 'My name is Alan!');
+
+****************************************************************/
--- notes.js
+++ notes.js
@@ -1,5 +1,37 @@
 console.log('Starting notes.js');
 
+let addNote = (title, body) => {
+  console.log('Adding note', title, body)
+};
+
+let readNote = (title) => {
+  console.log('Reading note', title)
+};
+
+let removeNote = (title) => {
+  console.log('Removing note', title)
+};
+
+let getAll = () => {
+  console.log('Getting all notes')
+};
+
+module.exports = {
+  addNote,
+  readNote,
+  removeNote,
+  getAll
+}
+
+
+
+
+
+
+
+
+/*********************************************************
+
 module.exports.age = 46;
 
 module.exports.addNote = () => {
@@ -11,3 +43,5 @@ module.exports.add = (a, b) => {
   console.log('add');
   return a + b;
 }
+
+*********************************************************/
